69b751c04c07f7ee00e9503a9b09bff7c98c6cae
KobbyMenz <augustinemensah09@gmail.com>
2025-08-31 14:33:21 +0000

    deploy app

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
       initial={{ y: -100 }}
       animate={{ y: 0 }}
       transition={{ duration: 0.5 }}
-      className="bg-background/2 border-b backdrop-blur-xl border-border fixed w-full top-0 z-50"
+      className="bg-background border-b backdrop-blur-xl border-border fixed w-full top-0 z-50"
     >
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between ">
         <div className="flex items-center space-x-3">
